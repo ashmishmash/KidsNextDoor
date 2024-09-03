2600ac25bfb38cba6b1aec7366e20bc59cd2f70a
RamzCas <2749414@students.wits.ac.za>
2024-09-03 12:05:18 +0200

    mechanic at work

--- Shadow Wizard Money Gang/Assets/Scripts/Lights/LightTriggerPoint.cs
+++ Shadow Wizard Money Gang/Assets/Scripts/Lights/LightTriggerPoint.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class LightTriggerPoint : MonoBehaviour
 {
-public Light Light;
+    public Light Light;
+    public bool LightOn;
+    public float maxLight;
+    public float MidLight;
+    public float minLight;
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player")) 
         {
-            Debug.Log("light on");
-            Light.enabled = true;
-            Destroy(this.gameObject);
+            //Debug.Log("light on");
+            //Light.enabled = true;
+            //Destroy(this.gameObject);
+            LightOn = true;
         }
     }
+
+    private void Update()
+    {
+        if (LightOn) 
+        {
+            StartCoroutine(FlameOn());
+            //Debug.Log("light on");
+        }
+    }
+
+    public IEnumerator FlameOn() 
+    {
+        Light.enabled = true;
+        Light.intensity = maxLight;
+        yield return new WaitForSeconds(5);
+        Light.intensity = MidLight;
+        yield return new WaitForSeconds(5);
+        Light.intensity = minLight;
+        yield return new WaitForSeconds(5);
+        Light.enabled = false;
+        LightOn = false;
+    }
+    
 }
