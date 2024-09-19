4dcb270882c0d641b5f8a52abee934aa04460250
RamzCas <2749414@students.wits.ac.za>
2024-09-19 20:58:46 +0200

    All Puzzles have a key

--- Shadow Wizard Money Gang/Assets/Scripts/RotatingDoor/TriggerPointCheck.cs
+++ Shadow Wizard Money Gang/Assets/Scripts/RotatingDoor/TriggerPointCheck.cs
@@ -7,11 +7,12 @@ public class TriggerPointCheck : MonoBehaviour
     public DoorTriggerPointOne Pt1;
     public DoorTriggerPointTwo Pt2;
     public DoorTriggerPointThree Pt3;
-    public GameObject Tp;
+    //public GameObject Tp;
     public GameObject ShootTrgger1;
     public GameObject ShootTrgger2;
     public GameObject ShootTrgger3;
-    public GameObject ConfetyCanon1;
+    public GameObject Key;
+   /* public GameObject ConfetyCanon1;
     public GameObject ConfetyCanon2;
     public GameObject Pic1;
     public GameObject Pic2;
@@ -19,7 +20,7 @@ public class TriggerPointCheck : MonoBehaviour
     public GameObject Pic4;
     public GameObject CatPic;
     public GameObject CatPic2;
-    public GameObject CatPic3;
+    public GameObject CatPic3;*/
     void Update()
     {
         if (Pt1.PointOneOpen) 
@@ -29,11 +30,12 @@ public class TriggerPointCheck : MonoBehaviour
             if(Pt3.PointThreeOpen) 
                 {
                     //Debug.Log("Open");
-                    Tp.SetActive(true);
+                    //Tp.SetActive(true);
                     ShootTrgger1.SetActive(false);
                     ShootTrgger2.SetActive(false);
                     ShootTrgger3.SetActive(false);
-                    ConfetyCanon1.SetActive(true);
+                    Key.SetActive(true);
+                    /*ConfetyCanon1.SetActive(true);
                     ConfetyCanon2.SetActive(true);
                     Pic1.SetActive(true);
                     Pic2.SetActive(true);
@@ -41,7 +43,7 @@ public class TriggerPointCheck : MonoBehaviour
                     Pic4.SetActive(true);
                     CatPic.SetActive(false);
                     CatPic2.SetActive(false);
-                    CatPic3 .SetActive(false);
+                    CatPic3 .SetActive(false);*/
                 }
             }
         }
