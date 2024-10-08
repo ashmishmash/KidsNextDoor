c7ef8633a0e1e4a1bb6bc473e214598f00540a1a
Ashley <ashleyclairetanton@icloud.com>
2024-10-08 11:44:28 +0200

    tutorial triggers

---
+++ Shadow Wizard Money Gang/Assets/Scripts/TutorialTriggers.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTriggers : MonoBehaviour
+{
+    public GameObject[] Triggers;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("MoveLook"))
+        {
+            MoveLook();
+        }
+
+        if (CompareTag("Sprint"))
+        {
+            Sprint();
+        }
+
+        if (CompareTag("Jump"))
+        {
+            Jump();
+        }
+
+        if (CompareTag("Crouch"))
+        {
+            Crouch();
+        }
+    }
+    public void MoveLook()
+    {
+        Triggers[0].SetActive(false);
+        Triggers[1].SetActive(false);
+    }
+
+    public void Sprint()
+    {
+        Triggers[2].SetActive(true);
+        StartCoroutine(WaitToGo());
+    }
+
+    public void Jump()
+    {
+        Triggers[3].SetActive(true);
+        StartCoroutine(WaitToGo());
+    }
+
+    public void Crouch()
+    {
+        Triggers[4].SetActive(true);
+        StartCoroutine(WaitToGo());
+    }
+
+    public IEnumerator WaitToGo()
+    {
+        yield return new WaitForSeconds(2f);
+        Triggers[2].SetActive(false);
+        Triggers[3].SetActive(false);
+        Triggers[4].SetActive(false);
+    }
+}
