e16fcbcb5af803d01a233241a74cfe79748159dc
Ashley <ashleyclairetanton@icloud.com>
2024-10-08 11:10:36 +0200

    pause stuff

--- Shadow Wizard Money Gang/Assets/Scripts/ButtonControls.cs
+++ Shadow Wizard Money Gang/Assets/Scripts/ButtonControls.cs
@@ -5,21 +5,53 @@ using UnityEngine;
 public class ButtonControls : MonoBehaviour
 {
     public PauseMenuScript PauseMenuScript;
-    //public FirstPersonControls FirstPersonControls;
+    public FirstPersonControls FirstPersonControls;
     public GameObject pauseImage;
     public CharacterController characterController;
     public FirstPersonControls controls;
-   public void Resume() 
+
+    public Animator tscrollAnim;
+    public Animator bscrollAnim;
+    public Animator pageAnim;
+    public Animator pawsAnim;
+    public void Resume() 
     {
-        Debug.Log("PLay");
+        
+       
+        tscrollAnim.SetFloat("Speed",-1f);
+        tscrollAnim.enabled = true;
+        bscrollAnim.SetFloat("Speed", -1f);
+        bscrollAnim.enabled = true;
+        //bscrollAnim.Play("bottom scroll open");
+        pageAnim.SetFloat("Speed", -1f);
+        pageAnim.enabled = true;
+        //pageAnim.Play("page");
+        pawsAnim.SetFloat("Speed", -1f);
+        pawsAnim.enabled = true;
+        //pawsAnim.Play("paws");
+
+        
+        Debug.Log("Play");
+        StartCoroutine(WaitToPlay());
+       
+    }
+
+    public IEnumerator WaitToPlay()
+    {  Debug.Log("Waited");
+        yield return new WaitForSeconds(2f);
+      
+        tscrollAnim.enabled = false;
+        bscrollAnim.enabled = false;
+        pageAnim.enabled = false;
+        pawsAnim.enabled = false;
+
+        pauseImage.SetActive(false);
         PauseMenuScript.inputCount = 0;
-        //FirstPersonControls.enabled = true;
+        FirstPersonControls.enabled = true;
         characterController.enabled = true;
         controls.lookSpeed = 0.25f;
         Time.timeScale = 1;
         //FirstPersonControls.canShoot = true;
-        characterController.enabled = true;
-        pauseImage.SetActive(false);
 
     }
 }
