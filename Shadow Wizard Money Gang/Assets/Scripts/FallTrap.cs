c095a2c87af5c517fb3cb109760cc5ec3452c558
RamzCas <2749414@students.wits.ac.za>
2024-10-23 19:05:47 +0200

    visions added

--- Shadow Wizard Money Gang/Assets/Scripts/FallTrap.cs
+++ Shadow Wizard Money Gang/Assets/Scripts/FallTrap.cs
@@ -6,12 +6,28 @@ using UnityEngine.SceneManagement;
 
 public class FallTrap : MonoBehaviour
 {
+    [Header("controllers")]
     public CharacterController characterController;
     public FirstPersonControls controls;
 
+    [Header("cut scene animations")]
     public Animator camAnim;
     public ParticleSystem dust;
     public ParticleSystem rocks;
+
+    [Header("player objects")]
+    public GameObject laser;
+    public GameObject FallingObject;
+    public GameObject Player;
+
+    [Header("screen shots")]
+    public GameObject StartRoom;
+    public GameObject SymbolPuzzle;
+    public GameObject DoorPuzzle;
+    public GameObject Skull;
+    public GameObject CatNip;
+
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -28,6 +44,28 @@ public class FallTrap : MonoBehaviour
     public IEnumerator WaitToFall()
     {
         yield return new WaitForSeconds(2f);
+        //SceneManager.LoadSceneAsync(2);
+        camAnim.StopPlayback();
+        laser.SetActive(false);
+        Player.transform.position = FallingObject.transform.position;
+        characterController.enabled = true;
+        yield return new WaitForSeconds(1f);
+        //characterController.enabled = false;
+        StartRoom.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        StartRoom.SetActive(false);
+        SymbolPuzzle.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        SymbolPuzzle.SetActive(false);
+        DoorPuzzle.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        DoorPuzzle .SetActive(false);
+        Skull.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        Skull.SetActive(false);
+        CatNip.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        //CatNip .SetActive(false);
         SceneManager.LoadSceneAsync(2);
     }
 
